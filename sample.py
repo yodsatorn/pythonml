176878d642f1535aaff4e8e86e2d9ad823453913
Kanakorn Horsiritham <kanakorn.h@psu.ac.th>
2017-09-23 04:22:49 +0700

    add train_test_split

--- sample.py
+++ sample.py
@@ -1,7 +1,10 @@
 from sklearn import svm
 from sklearn import datasets
+from sklearn.model_selection import train_test_split
 
 iris = datasets.load_iris()
 
 clf = svm.SVC()
 print(clf.fit(iris.data, iris.target))
+
+train_test_split( iris.data, iris.target, test_size=0.4 , random_state=0)
